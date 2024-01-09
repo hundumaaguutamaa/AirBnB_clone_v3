a54d80d88e9a80a8f285e2359d7b26fa866432e4
hundumaaguutamaa <hundumaa.guutamaa@gmail.com>
2024-01-08 17:40:50 -0800

    index

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Returning a Json response. """
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def status_check()
+    return jsonify({"status": "OK"})
