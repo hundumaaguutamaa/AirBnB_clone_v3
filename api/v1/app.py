d53c50b57da0dac1a4deb26c80760e3c100e16da
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-01-09 03:59:36 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,9 +17,8 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     storage.close()
 
-if __name__ == "__main__":
-    # Run the Flask server with specified host and port
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+   
     app.run(host=host, port=port, threaded=True)
-
