28eed25ecd7b5671647f301a846b73dc29892931
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-01-09 05:01:29 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,13 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/api/v1/stats', methods=['GET'])
+@app_views.route('/status')
+def status_check():
+    return jsonify({"status": "OK"})
+    
+@app_views.route('/stats', methods=['GET'])
 def object_stats():
-    """ Use the count() method from storage to retrieve
-        the number of objects by type
-    """
+    """ Use the count() method to retrieve the number of objects by type"""
 
     objects = {
             "amenities": storage.count('Amenity'),
