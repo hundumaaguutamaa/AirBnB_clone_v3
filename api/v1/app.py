442dcd25d8cfb4eb63bf2d0b3a83f255300a1fe2
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2024-01-09 05:08:09 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,10 +12,18 @@ app = Flask(__name__)
 # Register a blueprint app_views to the Flask instance app
 app.register_blueprint(app_views)
 
+# Initialize CORS with the app instance
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 # Declare a method to handle @app.teardown_appcontext that calls storage.close()
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """ Handler for 404 Not found errors. """
+    return {'error': 'Not found'}, 404
 
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
